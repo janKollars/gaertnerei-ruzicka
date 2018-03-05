897d31d396a5c3ee7eef91d12b880f92ba21d8c9
janKollars <kollars.jan@gmail.com>
2018-03-05 23:51:46 +0100

    Datenbank eingebunden

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,35 @@ import jets from "jets"
 
 const search = new jets({
   searchTag: '#jetsSearch',
-  contentTag: '#jetsContent'
+  contentTag: '#jetsContent',
+  columns: [0]
 });
 
 const searchBox = document.getElementById('jetsSearch');
 const searchList = document.getElementById('jetsContent');
+const button = document.querySelector('.btn-expand');
+let expanded = false;
 
 searchBox.addEventListener('input', () => {
-  searchBox.style.marginBottom =(searchBox.value == "") ? '5rem' : '0';
-  searchList.style.display = (searchBox.value == "") ? 'none' : 'block';
+  if (expanded) hideList();
+  searchList.style.display = (searchBox.value == "") ? 'none' : 'table-row-group';
+  document.querySelector('.categories').style.display = (searchBox.value == "") ? 'block' : 'none';
+  button.disabled = (searchBox.value == "") ? false : true;
 });
+
+button.addEventListener('click', showList);
+function showList() {
+  expanded = true;
+  button.textContent = "Alle ausblenden";
+  searchList.style.display = 'table-row-group';
+  button.removeEventListener('click', showList);
+  button.addEventListener('click', hideList);
+}
+
+function hideList() {
+  expanded = false;
+  button.textContent = "Alle anzeigen";
+  searchList.style.display = 'none';
+  button.removeEventListener('click', hideList);
+  button.addEventListener('click', showList);
+}
